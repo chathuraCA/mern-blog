ad548b0b3dc1d2213c7ae3cbda45993f301f18d2
chathura <k11332.chathura@gmail.com>
2024-06-29 16:34:10 +0530

    reate test api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import userRoutes from "./routes/User.route.js";
 dotenv.config();
 mongoose
-  .connect(
-    process.env.MONGO
-  )
+  .connect(process.env.MONGO)
   .then(() => {
     console.log("Mongo db is connected");
-  }).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
   });
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running fffff on port 3000");
 });
+
+app.use("/api/user", userRoutes);
