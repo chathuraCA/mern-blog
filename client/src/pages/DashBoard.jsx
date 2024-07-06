eee541ac801929f822ea29348a17396cf0d82126
chathura <k11332.chathura@gmail.com>
2024-07-07 03:30:35 +0530

    complete side bar of dashbord

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import DashSidebar from "../component/DashSidebar.jsx";
+import DashProfile from "../component/DashProfile.jsx";
 export default function DashBoard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>DashBoard</div>
-  )
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
